ae5451ff7b18f6755ceb30178b5a471990e25ef3
Dawei Luo <545489747@qq.com>
2018-08-21 16:46:16 +0800

    Add some code to show broken case when the generate_ann_list method is running

--- WordAnnotationProcessor.py
+++ WordAnnotationProcessor.py
@@ -29,11 +29,6 @@ class WordAnnotationProcessor:
         self.index_list = self.word_info['index_list']
         self.ann_list = list()
 
-    def cut_words(self):
-        self.word_list = self.word_info['word_list']
-        self.pos_list = self.word_info['pos_list']
-        self.index_list = self.word_info['index_list']
-
     def generate_ann_list(self):
         for i in range(0, len(self.word_list)):
             # 若label_list为空或者不存在，单词为--”O“
@@ -42,7 +37,9 @@ class WordAnnotationProcessor:
                 continue
 
             result = list()
+            word = self.word_list[i]
 
+            # 将下列注释掉的代卖恢复，可以打印出文章中broken case的情况
             for label in self.label_list:
                 l_start = label['start']
                 l_end = label['end']
@@ -52,19 +49,34 @@ class WordAnnotationProcessor:
                 if w_end <= l_start or w_start >= l_end:
                     pass
                 elif w_start < l_start and w_end == l_end:
+                    # print('Mode A, Case No.1')
+                    # print('word: %s\t\tlabel: %s' % (word, label))
                     result.append('B-' + self._LABELS[label['label']])
                 elif w_start < l_start and w_end > l_end:
+                    # print('Mode A, Case No.3')
+                    # print('word: %s\t\tlabel: %s' % (word, label))
                     result.append('B-' + self._LABELS[label['label']])
                 elif w_start < l_start and w_end > l_start:
+                    # print('Mode B, Case No.4')
+                    # print('word: %s\t\tlabel: %s' % (word, label))
                     result.append('B-' + self._LABELS[label['label']])
                 elif w_start == l_start and w_end <= l_end:
                     result.append('B-' + self._LABELS[label['label']])
                 elif w_start == l_start and w_end > l_end:
+                    # print('Mode A, Case No.2')
+                    # print('word: %s\t\tlabel: %s' % (word, label))
                     result.append('B-' + self._LABELS[label['label']])
                 elif w_start > l_start and w_end <= l_end:
                     result.append('I-' + self._LABELS[label['label']])
                 elif w_start > l_start and w_start < l_end and w_end > l_end:
                     result.append('I-' + self._LABELS[label['label']])
+                    # index = util.find_begin_index(i - 1, self.ann_list)
+                    # if self.index_list[index]['start'] < l_start:
+                    #     print('---------\tMode B, Case No. 6\t---------')
+                    #     print('word: %s\t\tlabel: %s' % (word, label))
+                    # else:
+                    #     print('Mode B, Case No.5')
+                    #     print('word: %s\t\tlabel: %s' % (word, label))
 
             if len(result) == 0:
                 self.ann_list.append('O')
